2fbbc523883dbdbffd0ac2c7888a1d9840b6e29d
LIKITH-r-likith <likithr.ai23@bmsce.ac.in>
2025-03-12 15:56:07 +0530

    Add files via upload

---
+++ p2a.java
@@ -0,0 +1,33 @@
+public class p2a {
+    public static void main(String[] args){
+        int a =5;
+        Integer b = a ;
+        int c=b;
+        System.out.println("value of a="+a);
+        System.out.println("value of b="+b);
+        System.out.println("value of c="+c);
+
+        double a1 =5;
+        double b1 = a1 ;
+        double c1=b1;
+        System.out.println("value of a1="+a1);
+        System.out.println("value of b1="+b1);
+        System.out.println("value of c1="+c1);
+
+        byte x1=5;
+        Byte y1=x1;
+        byte z1=y1 ;
+        System.out.println("Value of x1 = "+x1);
+        System.out.println("Value of y1 = "+y1);
+        System.out.println("Value of z1 = "+z1);
+
+        char x2='a';
+        Character y2=x2;
+        char z2=y2 ;
+        System.out.println("Value of x2 = "+x2);
+        System.out.println("Value of y2 = "+y2);
+        System.out.println("Value of z2 = "+z2);
+
+    }
+
+}
